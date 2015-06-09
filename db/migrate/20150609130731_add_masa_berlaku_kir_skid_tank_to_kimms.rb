f0866728063d09707ad9ea954e43ec13d7a18544
hendranata <hendranatas@yahoo.com>
2015-06-09 20:31:46 +0700

    fix

--- db/migrate/20150609130731_add_masa_berlaku_kir_skid_tank_to_kimms.rb
+++ db/migrate/20150609130731_add_masa_berlaku_kir_skid_tank_to_kimms.rb
@@ -1,5 +1,5 @@
 class AddMasaBerlakuKirSkidTankToKimms < ActiveRecord::Migration
   def change
-  	add_column :kimms, :masa_berlaku_kir_skid_tank, :string
+  	add_column :kimms, :masa_berlaku_kir_skid_tank, :datetime
   end
 end
